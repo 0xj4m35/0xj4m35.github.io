587812798e8de85eca62926fe6d21ae53f2ef20f
James Nguyen <theuranus.ntb@gmail.com>
2021-08-27 20:31:49 +0700

    chore: add far cdn

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,16 +1,22 @@
-import Head from 'next/head'
-import { MainLayout } from '../components/layouts/MainLayout'
-import '../styles/index.css'
+import Head from 'next/head';
+import { MainLayout } from '../components/layouts/MainLayout';
+import '../styles/index.css';
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <MainLayout>
       <Head>
         <title>James Ng</title>
+        <link
+          rel='stylesheet'
+          href='https://pro.fontawesome.com/releases/v5.10.0/css/all.css'
+          integrity='sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p'
+          crossorigin='anonymous'
+        />
       </Head>
       <Component {...pageProps} />
     </MainLayout>
-  )
-}
+  );
+};
 
-export default MyApp
+export default MyApp;
