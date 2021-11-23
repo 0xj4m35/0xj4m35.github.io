d6bfe6a6407fe68d72274487f3e601b4cb431c05
James Nguyen <theuranus.ntb@gmail.com>
2021-11-24 00:40:37 +0700

    add: og and twitter card

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -13,6 +13,27 @@ const MyApp = ({ Component, pageProps }) => {
           integrity='sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p'
           crossOrigin='anonymous'
         />
+        {/* Twitter Card */}
+        <meta content='summary_large_image' name='twitter:card' />
+        <meta content='@james_ng_th' name='twitter:site' />
+        <meta content='The Uranus' name='twitter:title' />
+        <meta
+          content="The Uranus's personal website."
+          name='twitter:description'
+        />
+        <meta content='/meta-image.jpg' name='twitter:image' />
+        {/* Twitter Card */}
+
+        {/* Open Graph*/}
+        <meta content='The Uranus' property='og:title' />
+        <meta
+          content="The Uranus's personal website."
+          property='og:description'
+        />
+        <meta content='/meta-image.jpg' property='og:image' />
+        <meta content='website' property='og:type' />
+        <meta content='TheUranus' property='og:site_name' />
+        {/* Open Graph*/}
       </Head>
       <Component {...pageProps} />
     </MainLayout>
