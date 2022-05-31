61165a88ae45329930b1faff5454760f71108d6a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2022-05-31 07:49:17 +0000

    Deploy theuranus/theuranus.github.io to github.com/theuranus/theuranus.github.io.git:gh-pages

---
+++ _next/static/chunks/pages/index-eb901e7a7b52c255bb3c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{8894:function(n,u,t){"use strict";t.r(u);var _=t(5893),r=t(4228);u.default=function(){return(0,_.jsx)(r.Z,{})}},3685:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return t(8894)}])}},function(n){n.O(0,[228,774,888,179],(function(){return u=3685,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
